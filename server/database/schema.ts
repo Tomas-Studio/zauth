c0a0f29461a02474691322d374ca8970f7e26543
olakunle.olatunji <tjomas20@gmail.com>
2024-04-15 18:16:18 +0100

    chore: added @paralleldrive/cuid2

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,6 +1,16 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core'
+import { pgEnum, pgTable, serial, text } from 'drizzle-orm/pg-core'
+
+export const roleEnum = pgEnum('role', ['user', 'admin', 'manager'])
+export const authTypeEnum = pgEnum('auth_type', ['microsoft', 'google'])
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
-  email: text('email').notNull(),
+  email: text('email').notNull().unique(),
+  role: roleEnum('role').default('user').notNull(),
+  authType: authTypeEnum('auth_type').notNull(),
+})
+
+export const sessions = pgTable('sessions', {
+  id: serial('id').primaryKey(),
+
 })
