4c7f4720d47d6631f66db5787f96e4afc8eee323
olakunle.olatunji <tjomas20@gmail.com>
2024-04-15 02:28:55 +0100

    feat: generate migration and setup drizzle studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
   schema: './server/database/schema.ts',
   out: './server/database/migrations',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.NUXT_DATABASE_URL!,
+  },
 })
