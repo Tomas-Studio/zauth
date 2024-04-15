1fa8658912382b1cd2fc024fc988341d352bf5c4
olakunle.olatunji <tjomas20@gmail.com>
2024-04-15 01:42:00 +0100

    chore: modified user schema

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,5 +1,6 @@
-import { pgTable, serial } from 'drizzle-orm/pg-core'
+import { pgTable, serial, text } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
+  email: text('email').notNull(),
 })
